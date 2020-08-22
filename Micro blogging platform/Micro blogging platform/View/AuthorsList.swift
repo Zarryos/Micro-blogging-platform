834a1d1b292e6825cfc9b8f96a8762d3ffcb02ff
Florian LOUVET <flo.louvet94@gmail.com>
2020-08-22 14:47:48 +0200

    Base of MVC project

--- Micro blogging platform/Micro blogging platform/View/AuthorsList.swift
+++ Micro blogging platform/Micro blogging platform/View/AuthorsList.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  AuthorsList.swift
 //  Micro blogging platform
 //
 //  Created by user177985 on 8/22/20.
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AuthorsList: View {
     var body: some View {
-        Text("Hello, World!")
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct AuthorsList_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AuthorsList()
     }
 }

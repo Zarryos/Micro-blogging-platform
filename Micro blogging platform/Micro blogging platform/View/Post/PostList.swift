fd9b98e2af7859b3ae646ff8d0057933c6bca6d2
Florian LOUVET <flo.louvet94@gmail.com>
2020-08-22 15:33:32 +0200

    Add PostList

---
+++ Micro blogging platform/Micro blogging platform/View/Post/PostList.swift
@@ -0,0 +1,27 @@
+//
+//  PostList.swift
+//  Micro blogging platform
+//
+//  Created by user177985 on 8/22/20.
+//  Copyright © 2020 user177985. All rights reserved.
+//
+
+import SwiftUI
+
+struct PostList: View {
+    @ObservedObject var postsObservable = PostObservable()
+    
+    var body: some View {
+        Section(header: Text("List of posts").font(.title), content: {
+            List (postsObservable.posts) { post in
+                PostRow(post: post)
+            }
+        })
+    }
+}
+
+struct PostList_Previews: PreviewProvider {
+    static var previews: some View {
+        PostList()
+    }
+}

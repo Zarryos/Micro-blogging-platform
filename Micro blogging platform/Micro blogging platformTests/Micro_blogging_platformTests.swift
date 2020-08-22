255106ae47cadcc37e4617d86a1ba23166e3063e
Florian LOUVET <flo.louvet94@gmail.com>
2020-08-22 20:51:39 +0200

    Testing init of Post and Comment

--- Micro blogging platform/Micro blogging platformTests/Micro_blogging_platformTests.swift
+++ Micro blogging platform/Micro blogging platformTests/Micro_blogging_platformTests.swift
@@ -16,7 +16,7 @@ class ModelInitTests: XCTestCase {
         let name = "TestName"
         let userName = "TestUserName"
         let email = "test@email.com"
-        let avatarUrl = URL(string: "www.test.com")!
+        let avatarUrl = URL(string: "www.testAvatar.com")!
         let address = AuthorCoordinate(latitude: "0", longitude: "0")
         
         let author = Author.init(id: id, name: name, userName: userName, email: email, avatarUrl: avatarUrl, address: address)
@@ -26,5 +26,43 @@ class ModelInitTests: XCTestCase {
         XCTAssertEqual(author.userName, userName)
         XCTAssertEqual(author.email, email)
         XCTAssertEqual(author.avatarUrl, avatarUrl)
+    }
+    
+    func testInitPost() {
+        let id = 0
+        let date = "2017-09-07T09:58:50.972Z"
+        let title = "Test title"
+        let body = "Test body"
+        let imageUrl = URL(string: "www.testImage.com")!
+        let authorId = 0
+        
+        let post = Post.init(id: id, date: date, title: title, body: body, imageUrl: imageUrl, authorId: authorId)
+        
+        XCTAssertEqual(post.id, id)
+        XCTAssertEqual(post.date, date)
+        XCTAssertEqual(post.title, title)
+        XCTAssertEqual(post.body, body)
+        XCTAssertEqual(post.imageUrl, imageUrl)
+        XCTAssertEqual(post.authorId, authorId)
+    }
+    
+    func testInitComment() {
+        let id = 0
+        let date = "2017-09-07T09:58:50.972Z"
+        let body = "Test body"
+        let userName = "TestUserName"
+        let email = "test@email.com"
+        let avatarUrl = URL(string: "www.testAvatar.com")!
+        let postId = 0
+        
+        let comment = Comment.init(id: id, date: date, body: body, userName: userName, email: email, avatarUrl: avatarUrl, postId: postId)
+        
+        XCTAssertEqual(comment.id, id)
+        XCTAssertEqual(comment.date, date)
+        XCTAssertEqual(comment.body, body)
+        XCTAssertEqual(comment.userName, userName)
+        XCTAssertEqual(comment.email, email)
+        XCTAssertEqual(comment.avatarUrl, avatarUrl)
+        XCTAssertEqual(comment.postId, postId)
     }
 }

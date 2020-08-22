4044e32472010f1bcf86fca139887c1cb1637f5d
Florian LOUVET <flo.louvet94@gmail.com>
2020-08-22 19:04:34 +0200

    Commented Author struct

--- Micro blogging platform/Micro blogging platform/Model/Author.swift
+++ Micro blogging platform/Micro blogging platform/Model/Author.swift
@@ -8,14 +8,33 @@
 
 import Foundation
 
+/// An human who write Post
 struct Author: Decodable, Identifiable {
+    /// Unique identifier
     var id: Int
+    /// Name
     var name: String
+    /// UserName
     var userName: String
+    /// Email
     var email: String
+    /// Avatar URL, expecting a picture from it
     var avatarUrl: URL
+    /// Author coordinates
     var address: AuthorCoordinate
     
+    /**
+     Initializes a new Author with the provided attributes
+      - Parameters:
+        - id: It's unique identifier
+        - name: The author name
+        - userName: The author user name
+        - email: The author email
+        - avatarUrl: The author avatar URL, that store the avatar picture
+        - address: The author coordinates
+     
+     - Returns: A simple author object to work with
+     */
     init(id: Int, name: String, userName: String, email: String, avatarUrl: URL, address: AuthorCoordinate) {
         self.id = id
         self.name = name
@@ -25,6 +44,7 @@ struct Author: Decodable, Identifiable {
         self.address = address
     }
     
+    /// A mapping between the author attributes and the fields in the Json response
     enum CodigKeys: String, CodingKey {
         case id = "id"
         case name = "name"
@@ -35,14 +55,25 @@ struct Author: Decodable, Identifiable {
     }
 }
 
+/// A set of latitude and longitude dedicated to the author
 struct AuthorCoordinate: Decodable {
+    /// Latitude and longitude
     let latitude, longitude: String
     
+    /**
+     Initializes a new AuthorCoordinates with the provided attributes
+      - Parameters:
+        - latitude: Angular distance from the author location to the equator
+        - longitude: Angular distance from the author location to a prime meridian
+     
+     - Returns: The author coordinates
+     */
     init(latitude: String, longitude: String) {
         self.latitude = latitude
         self.longitude = longitude
     }
     
+    /// A mapping between the author coordinates attributes and the fields in the Json response
     enum CodigKeys: String, CodingKey {
         case latitude = "latitude"
         case longitude = "longitude"
